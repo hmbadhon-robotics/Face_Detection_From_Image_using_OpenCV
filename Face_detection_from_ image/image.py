b53cdaa85f0a6fa384ad68f9bedd1a2915e5ff74
Ba_Bot <111107434+hmbadhon-robotics@users.noreply.github.com>
2022-12-13 12:58:46 +0600

    Add files via upload

---
+++ Face_detection_from_ image/image.py
@@ -0,0 +1,20 @@
+import cv2
+
+# Load the cascade classifier
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# Read the input image
+image = cv2.imread('w.jpg')
+
+# Convert the image to grayscale
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Detect faces in the image
+faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+# Draw a rectangle around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+# Save the output image
+cv2.imwrite('output.jpg', image)
